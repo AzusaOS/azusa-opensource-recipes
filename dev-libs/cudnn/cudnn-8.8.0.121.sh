1b0612f5ed12846335d70eb08bee125e14fae996
Mark Karpeles <magicaltux@gmail.com>
2025-02-03 20:53:40 +0900

    bump cudnn-8.8.0.121.sh

--- dev-libs/cudnn/cudnn-8.8.0.121.sh
+++ dev-libs/cudnn/cudnn-8.8.0.121.sh
@@ -2,11 +2,11 @@
 source "../../common/init.sh"
 
 # supports 11.x but URL has a specific version number
-CUDA_MA="11"
+CUDA_MA="12"
 CUDA_MI="8"
 CUDA_V="${CUDA_MA}.${CUDA_MI}"
 
-get https://developer.download.nvidia.com/compute/redist/cudnn/v${PV%.*}/local_installers/${CUDA_V}/cudnn-linux-x86_64-${PV}_cuda${CUDA_MA}-archive.tar.xz
+get https://developer.download.nvidia.com/compute/cudnn/redist/cudnn/linux-x86_64/cudnn-linux-x86_64-${PV}_cuda${CUDA_MA}-archive.tar.xz
 acheck
 
 cd "${S}"
