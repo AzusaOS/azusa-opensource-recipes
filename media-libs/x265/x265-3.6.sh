94acd38cb568ee8c1d783576330540fcdee842db
Mark Karpeles <magicaltux@gmail.com>
2025-03-21 21:52:14 +0900

    bump x265

--- media-libs/x265/x265-3.6.sh
+++ media-libs/x265/x265-3.6.sh
@@ -11,7 +11,4 @@ export CPPFLAGS="-DPIC -fPIC"
 docmake -DENABLE_SHARED=ON -DENABLE_HDR10_PLUS=ON -DHIGH_BIT_DEPTH=ON -DLIB_INSTALL_DIR=lib$LIB_SUFFIX -DCMAKE_LIBRARY_PATH=/pkg/main/sys-libs.glibc.libs/lib$LIB_SUFFIX "${CHPATH}"/*/source
 # -DENABLE_SVT_HEVC=ON -DENABLE_LIBVMAF=ON
 
-make
-make install DESTDIR="${D}"
-
 finalize
