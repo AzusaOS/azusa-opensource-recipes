04a6f42831ae21bd146376ed1e19774b28b57255
Mark Karpeles <magicaltux@gmail.com>
2025-03-31 20:43:40 +0900

    adding bsc-erigon 1.3.4

---
+++ net-p2p/bsc-erigon/bsc-erigon-1.3.4.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+source "../../common/init.sh"
+
+fetchgit https://github.com/node-real/bsc-erigon.git v${PV}
+
+envcheck
+# do not use acheck so we keep networking
+
+export PATH="/pkg/main/dev-lang.go.dev/bin:$PATH"
+export GOBIN="${D}/pkg/main/${PKG}.core.${PVRF}/bin"
+mkdir -pv "$GOBIN"
+
+cd "${S}"
+apatch "$FILESDIR/snapcfg_ignore_fail.patch"
+make erigon
+make install
+
+mv -v ./build/dist/erigon "${D}/pkg/main/${PKG}.core.${PVRF}/bin"
+mkdir -pv "${D}/pkg/main/${PKG}.core.${PVRF}/libexec"
+mv -v ./build/dist/libsilkworm_capi.so "${D}/pkg/main/${PKG}.core.${PVRF}/libexec"
+patchelf --add-rpath '$ORIGIN/../libexec' "${D}/pkg/main/${PKG}.core.${PVRF}/bin/erigon"
+
+finalize
