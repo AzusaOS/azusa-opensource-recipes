a6dfe5166d59687360c6ff63c1b87435feae04d1
Mark Karpeles <magicaltux@gmail.com>
2025-04-01 17:07:05 +0900

    bump bsc-erigon-1.3.4.1.sh using current main branch

--- net-p2p/bsc-erigon/bsc-erigon-1.3.4.1.sh
+++ net-p2p/bsc-erigon/bsc-erigon-1.3.4.1.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 source "../../common/init.sh"
 
-fetchgit https://github.com/node-real/bsc-erigon.git v${PV}
+# hardcoded revision
+fetchgit https://github.com/node-real/bsc-erigon.git 78a576f
 
 envcheck
 # do not use acheck so we keep networking
@@ -11,12 +12,13 @@ export GOBIN="${D}/pkg/main/${PKG}.core.${PVRF}/bin"
 mkdir -pv "$GOBIN"
 
 cd "${S}"
-apatch "$FILESDIR/snapcfg_ignore_fail.patch"
 make erigon
+make integration
 make install
 
 mv -v ./build/dist/erigon "${D}/pkg/main/${PKG}.core.${PVRF}/bin"
 mkdir -pv "${D}/pkg/main/${PKG}.core.${PVRF}/libexec"
+mv -v ./build/bin/integration "${D}/pkg/main/${PKG}.core.${PVRF}/libexec"
 mv -v ./build/dist/libsilkworm_capi.so "${D}/pkg/main/${PKG}.core.${PVRF}/libexec"
 patchelf --add-rpath '$ORIGIN/../libexec' "${D}/pkg/main/${PKG}.core.${PVRF}/bin/erigon"
 
